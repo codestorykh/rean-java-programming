2cd4658685599a96fea3c0c66d253386a697d742
Theara Dawn <dawntheara.me@gmail.com>
2022-03-28 19:43:16 +0700

    What's data type in java?

---
+++ variable and data type/src/com/rean/model/Animal.java
@@ -0,0 +1,29 @@
+package com.rean.model;
+
+public class Animal {
+
+    private String name;
+    private String firstName;
+    private String _s;
+    private static final Double MAX = 928_232_9.0;
+    private boolean s;
+    private int min;
+
+
+    public static void main(String[] args) {
+        System.out.println(MAX);
+    }
+
+    /**
+     *
+     * @param a
+     * @param b
+     * @param c
+     * @return
+     */
+    boolean test(int a, int b, int c) {
+        if(a == (b + c))
+            return true;
+        return false;
+    }
+}
